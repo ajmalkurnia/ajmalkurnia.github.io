958cc4889b640b65676e6dbd1bba0be1f51af7ee
ajmalkurnia <ajmalprayoga@gmail.com>
2023-05-29 19:33:28 +0700

    Update content

--- assets/js/main.js
+++ assets/js/main.js
@@ -48,4 +48,15 @@
     pickColor(e);
   }))
 
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+          });
+      });
+  });
+
+
 })()
